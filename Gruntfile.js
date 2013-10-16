3eaa5e056efdfecacc04ed6a910a9ef371250968
Abraham Haskins <abeisgreat@abeisgreat.com>
2013-10-16 01:33:48 -0500

    Added Odometer for animations, removed unused styles and methods, cleaned up build process, cleaned up file hierarchy, updated README and LICENSE, added bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,43 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-     jshint: {
+    bower: {
+      install: {
+        options: {
+          targetDir: 'src/lib'
+        }
+      }
+    },
+    concat: {
+      "btcquote-widget": {
+       "src": [
+         "src/*.js",
+         "src/lib/odometer/odometer.js"
+       ],
+        "dest": "build/btcquote-widget.js"
+      }
+    },
+    jshint: {
       all: [
-        'btcquote-widget.js',
+        'src/*.js',
       ]
     },
     uglify: {
-      options: {
-        mangle: false
-      },
       "btcquote-widget.min.js": {
-        src: "btcquote-widget.js",
-        dest: "btcquote-widget.min.js"
+        src: "build/btcquote-widget.js",
+        dest: "build/btcquote-widget.min.js"
       },
-    }});
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', [
     'jshint',
+    'bower',
+    'concat',
     'uglify'
   ]);
 };
